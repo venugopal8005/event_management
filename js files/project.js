3d77e24eac0a9a887691be246e3cc4c9e9b25704
sankar-talatam <shankartalatam252@gmail.com>
2024-07-27 14:07:57 +0530

    cards

---
+++ js files/project.js
@@ -0,0 +1,113 @@
+function fun1(){
+    
+    document.querySelectorAll(".overs").forEach(
+        function(val){
+            val.style.display = "none"
+        }
+    )
+    var ve=document.querySelector(".over1");
+    ve.style.display="block";
+
+    document.querySelector(".over").style.display="block";
+    var con=document.querySelector(".wrapper");
+    con.style.justifyContent= "start";
+}function fun2(){
+    document.querySelectorAll(".overs").forEach(
+        function(val){
+            val.style.display = "none"
+        }
+    )
+    var ve=document.querySelector(".over2");
+    ve.style.display="block";
+  
+    document.querySelector(".over").style.display="block";
+    var con=document.querySelector(".wrapper");
+    con.style.justifyContent= "start";
+}
+function fun3(){
+    document.querySelectorAll(".overs").forEach(
+        function(val){
+            val.style.display = "none"
+        }
+    )
+    var ve=document.querySelector(".over3");
+    ve.style.display="block";
+    
+
+    document.querySelector(".over").style.display="block";
+    var con=document.querySelector(".wrapper");
+    con.style.justifyContent= "start";
+}
+function fun4(){
+    document.querySelectorAll(".overs").forEach(
+        function(val){
+            val.style.display = "none"
+        }
+    )
+    var ve=document.querySelector(".over4");
+    ve.style.display="block";
+   
+
+    document.querySelector(".over").style.display="block";
+    var con=document.querySelector(".wrapper");
+    con.style.justifyContent= "start";
+}function fun5(){
+    document.querySelectorAll(".overs").forEach(
+        function(val){
+            val.style.display = "none"
+        }
+    )
+    var ve=document.querySelector(".over5");
+    ve.style.display="block";
+  
+
+    document.querySelector(".over").style.display="block";
+    var con=document.querySelector(".wrapper");
+    con.style.justifyContent= "start";
+}function fun6(){
+    document.querySelectorAll(".overs").forEach(
+        function(val){
+            val.style.display = "none"
+        }
+    )
+    var ve=document.querySelector(".over6");
+    ve.style.display="block";
+ 
+
+    document.querySelector(".over").style.display="block";
+    var con=document.querySelector(".wrapper");
+    con.style.justifyContent= "start";
+}
+function remove1(){
+    var one=document.querySelector(".over")
+    one.style.display="none";
+}
+function remove2(){
+    var one=document.querySelector(".over")
+    one.style.display="none";
+}
+function remove3(){
+    var one=document.querySelector(".over")
+    one.style.display="none";
+}
+function remove4(){
+    var one=document.querySelector(".over")
+    one.style.display="none";
+}
+function remove5(){
+    var one=document.querySelector(".over")
+    one.style.display="none";
+}
+function remove6(){
+    var one=document.querySelector(".over")
+    one.style.display="none";
+}
+function about(){
+    var oke=document.querySelector(".video");
+    oke.style.display="none";
+    console.log("sssfbsb");
+}
+// function about{
+//     var when=document.querySelector("body")
+//     when.style.backgroundColor="red";
+// }
